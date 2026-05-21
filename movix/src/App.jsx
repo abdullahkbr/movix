37685386ad7561fb37194185ecb30b49c340de36
abdullahProfile <abdullahdew88@gmail.com>
2026-05-22 00:21:57 +0500

    aos animations

--- movix/src/App.jsx
+++ movix/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import logo from "./assets/logo.png";
 import searchIcon from "./assets/search.png";
 import notificationIcon from "./assets/notification.png";
@@ -36,6 +39,12 @@ import playMusicIcon from "./assets/playmusic.png";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
   return (
     <>
       <header className="App-header">
@@ -72,122 +81,124 @@ function App() {
 
       <section className="hero">
         <div className="hero-container">
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m1} alt="Movie 1" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m2} alt="Movie 2" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m3} alt="Movie 3" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m4} alt="Movie 4" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m5} alt="Movie 5" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up ">
             <img src={m6} alt="Movie 6" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m7} alt="Movie 7" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m8} alt="Movie 8" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m9} alt="Movie 9" />
           </div>
 
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m10} alt="Movie 10" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m11} alt="Movie 11" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m12} alt="Movie 12" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m13} alt="Movie 13" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m14} alt="Movie 14" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m15} alt="Movie 15" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m16} alt="Movie 16" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m17} alt="Movie 17" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m18} alt="Movie 18" />
           </div>
 
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m19} alt="Movie 19" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m20} alt="Movie 20" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m21} alt="Movie 21" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m22} alt="Movie 22" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m23} alt="Movie 23" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m24} alt="Movie 24" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m25} alt="Movie 25" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m26} alt="Movie 26" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m27} alt="Movie 27" />
           </div>
 
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m10} alt="Movie 10" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m11} alt="Movie 11" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m12} alt="Movie 12" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m13} alt="Movie 13" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m14} alt="Movie 14" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m15} alt="Movie 15" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m16} alt="Movie 16" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m17} alt="Movie 17" />
           </div>
-          <div className="hero-image">
+          <div className="hero-image" aos-delay="100" data-aos="fade-up">
             <img src={m18} alt="Movie 18" />
           </div>
         </div>
 
         <div className="hero-content">
-          <h1>The Best Streaming Experience.</h1>
-          <p>
+          <h1 data-aos-delay="100" data-aos="fade-up">
+            The Best Streaming Experience.
+          </h1>
+          <p data-aos-delay="100" data-aos="fade-up">
             StreamVibe is the best streaming experience for watching your
             favorite movies and shows on demand, anytime, anywhere. With
             StreamVibe, you can enjoy a wide variety of content, including the
@@ -195,11 +206,15 @@ function App() {
             can also create your own watchlists, so you can easily find the
             content you want to watch..
           </p>
-          <button className="btn">
+          <button className="btn" data-aos-delay="100" data-aos="fade-up">
             <img src={playMusicIcon} alt="Play Music" />
             <span>Watch Now</span>
           </button>
         </div>
+      </section>
+
+      <section className="slider">
+        <div className="slider-container"></div>
       </section>
     </>
   );
